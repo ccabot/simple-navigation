29c4365bc55b821f1f91fdd6b3dbbf0fd5b89ba6
toby cabot <ccabot@mitre.org>
2010-12-08 14:38:21 -0500

    added a renderer called :names_only which renders a list of item names in raw text
    It's based on the breadcrumb renderer but renders in text instead of
    html.  This makes it useful for page titles.

--- lib/simple_navigation/rendering.rb
+++ lib/simple_navigation/rendering.rb
@@ -5,6 +5,7 @@ module SimpleNavigation
   module Renderer
     autoload :List, 'simple_navigation/rendering/renderer/list'
     autoload :Links, 'simple_navigation/rendering/renderer/links'
+    autoload :NamesOnly, 'simple_navigation/rendering/renderer/names_only'
     autoload :Breadcrumbs, 'simple_navigation/rendering/renderer/breadcrumbs'
   end
 end
